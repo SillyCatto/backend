c92200f3c65cf3beef062c64782dec181d39e364
SillyCatto <raiyanmuhtasim@gmail.com>
2024-12-14 11:45:27 +0600

    added some basic pages for testing

--- src/app.js
+++ src/app.js
@@ -3,9 +3,17 @@ const express = require('express');
 const port = 3000;
 const app = express();
 
+app.use('/hello', (req, res) => {
+    res.send('hello!');
+});
+
+app.use('/test', (req, res) => {
+    res.send('test page');
+});
+
 app.use((req, res) => {
     res.send('hello from the server');
-})
+});
 
 app.listen(port, () => {
     console.log(`server running on port: ${port}`);
