aa31845ce0906149b65029728dd48a52b4bdb01c
SillyCatto <raiyanmuhtasim@gmail.com>
2024-12-14 18:46:23 +0600

    testing dynamic routing

--- src/app.js
+++ src/app.js
@@ -5,10 +5,15 @@ const app = express();
 
 app.get('/user', (req, res) => {
     const data = {
-        name: 'Cat',
+        name: 'Bob',
         age: 10
     };
     res.send(data);
+});
+
+app.get('/user/:username/:age', (req, res) => {
+    console.log(req.params);
+    res.send('got params');
 });
 
 app.post('/user', (req, res) => {
@@ -17,7 +22,7 @@ app.post('/user', (req, res) => {
 })
 
 app.use('/test', (req, res) => {
-    res.send('test page\nAPI returned:\n');
+    res.send('test page');
 });
 
 app.listen(port, () => {
