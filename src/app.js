866afd5999329ed4d6dad19a812f84f26d08a421
SillyCatto <raiyanmuhtasim@gmail.com>
2024-12-14 23:43:09 +0600

    trying wild card error handler

--- src/app.js
+++ src/app.js
@@ -11,6 +11,21 @@ app.get('/getUserData', (req, res) => {
     // res.send('user data sent');
 });
 
+/**
+ * always use try-catch block, still you may have some unhandled
+ * errors, dont just only throw them and keep it that way.
+ * Cuz it will expose your app internals to client side.
+ * That is bad >:(
+ * instead, create a wild card error handler like below
+ * and handle it gracefully
+ */
+app.use('/', (err, req, res, next) => {
+    if (err) {
+        // log the error and notify / send alert
+        res.status(500).send('something went wrong :(');
+    }
+});
+
 app.listen(port, () => {
     console.log(`server running on port: ${port}`);
 });
