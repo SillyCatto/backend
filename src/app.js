4b3bc19257d438f35c73e2af2dcb3d321c5890b7
SillyCatto <raiyanmuhtasim@gmail.com>
2024-12-15 11:42:06 +0600

    created database connector

--- src/app.js
+++ src/app.js
@@ -1,31 +1,18 @@
 const express = require('express');
+const connectDB = require('./config/database');
 
 const port = 3000;
 const app = express();
 
-// this code is bad, it exposes your app files, etc
-app.get('/getUserData', (req, res) => {
-    // ... logic for DB calls ... etc
-    // ...
-    throw new Error('dummy error');
-    // res.send('user data sent');
-});
+connectDB()
+    .then(() => {
+        console.log('Database connected successfully.');
+        app.listen(port, () => {
+            console.log(`server running on port: ${port}`);
+        });
+    })
+    .catch((err) => {
+        console.error('Error connecting to database');
+    });
 
-/**
- * always use try-catch block, still you may have some unhandled
- * errors, dont just only throw them and keep it that way.
- * Cuz it will expose your app internals to client side.
- * That is bad >:(
- * instead, create a wild card error handler like below
- * and handle it gracefully
- */
-app.use('/', (err, req, res, next) => {
-    if (err) {
-        // log the error and notify / send alert
-        res.status(500).send('something went wrong :(');
-    }
-});
 
-app.listen(port, () => {
-    console.log(`server running on port: ${port}`);
-});
---
+++ src/config/database.js
@@ -0,0 +1,10 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
+
+const uri = process.env.MONGODB_URI;
+
+const connectDB = async () => {
+    await mongoose.connect(uri);
+}
+
+module.exports = connectDB;
