2d43047d8d3be0c7d9a3aa1fc13dd73a3a5e3589
SillyCatto <raiyanmuhtasim@gmail.com>
2024-12-15 20:11:02 +0600

    find user by email

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const port = 3000;
 const app = express();
 
 app.use(express.json());
+
 app.post('/signup', async (req, res) => {
     const user = new User(req.body);
 
@@ -17,6 +18,18 @@ app.post('/signup', async (req, res) => {
     }
 
 });
+
+// get user by email
+app.get('/user', async (req, res) => {
+    const userEmail = req.body.emailID;
+
+    try {
+        const user = await User.find({ emailID: userEmail });
+        res.send(user);
+    } catch (err) {
+        res.status(400).send('something went wrong :(');
+    }
+})
 
 connectDB()
     .then(() => {
