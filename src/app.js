391fc2b734dabb4147ad636de59b7f845e4e38fb
SillyCatto <raiyanmuhtasim@gmail.com>
2024-12-15 14:20:00 +0600

    adding dummy data to DB

--- src/app.js
+++ src/app.js
@@ -1,8 +1,22 @@
 const express = require('express');
 const connectDB = require('./config/database');
+const User = require('./models/user');
 
 const port = 3000;
 const app = express();
+
+app.post('/signup', async (req, res) => {
+    const userData = {
+        name: 'Bob',
+        emailID: 'bob@gmail.com',
+        password: '123',
+        age: 20,
+        gender: 'male'
+    };
+    const user = new User(userData);
+    await user.save();
+    res.send('user data added successfully');
+});
 
 connectDB()
     .then(() => {
