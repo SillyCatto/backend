de400a877da697a6f626dd388c30b93df816e903
SillyCatto <raiyanmuhtasim@gmail.com>
2024-12-17 12:04:30 +0600

    implemented dummy token auth

--- src/app.js
+++ src/app.js
@@ -1,15 +1,20 @@
 const express = require("express");
+const cookieParser = require("cookie-parser");
 
 const connectDB = require("./config/database");
 const User = require("./models/user");
-const { validateSignupInput, validateLoginInput } = require("./utils/validate");
+const {
+  validateSignupInput,
+  validateLoginInput,
+} = require("./utils/validateInput");
 const { encryptPassword } = require("./utils/encryptor");
-const { authUser } = require("./utils/auth");
+const { authUser } = require("./controller/auth");
 
 const port = 3000;
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.post("/signup", async (req, res) => {
   try {
@@ -34,6 +39,7 @@ app.post("/login", async (req, res) => {
     const isLoginSuccessful = await authUser(email, password);
 
     if (isLoginSuccessful) {
+      res.cookie("token", "saganigadik");
       res.send("Login successful!");
     } else {
       throw new Error("Invalid credentials");
@@ -51,6 +57,12 @@ app.get("/allUsers", async (req, res) => {
   } catch (err) {
     res.status(400).send("something went wrong :(");
   }
+});
+
+app.get("/profile", (req, res) => {
+  const cookies = req.cookies;
+  console.log(cookies);
+  res.send("reading cookies");
 });
 
 app.delete("/deleteUserByName", async (req, res) => {
